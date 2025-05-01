963648689e9a481e451c60638b7357dee1684451
SmithPatrick6000c <patdousmi@gmail.com>
2025-04-30 20:30:21 -0400

    Ball is displayed on Screen

--- main.c
+++ main.c
@@ -11,10 +11,10 @@ int main(){
     SDL_Window* window = SDL_CreateWindow("Ball Time", SDL_WINDOWPOS_UNDEFINED, SDL_WINDOWPOS_UNDEFINED,
                                 SCREEN_WIDTH, SCREEN_HEIGHT, SDL_WINDOW_SHOWN);
     SDL_Renderer* renderer = SDL_CreateRenderer(window, -1, SDL_RENDERER_ACCELERATED);
-    //Should Hack it to stay up
-    SDL_Event e; bool quit = false; while(quit == false){
+    //Should Hack it to stay up May need to comment
+    /*SDL_Event e; bool quit = false; while(quit == false){
             while(SDL_PollEvent(&e)){if(e.type == SDL_QUIT) quit = true;}}
-
+    */
 
     //creates Ball Structure
     typedef struct {
@@ -24,7 +24,27 @@ int main(){
     } Ball;
     Ball ball = {400, 300, 20, 5, 5};
 
+    //Game Section
 
+
+
+    //Clear Screen
+    SDL_SetRenderDrawColor(renderer, 0, 0, 0, 255);
+    SDL_RenderClear(renderer);
+    //Ball Draw
+    SDL_SetRenderDrawColor(renderer, 255, 255, 255, 255);
+    int br = ball.radius;
+    for(int i = -br; i <= br; i++){
+        for(int j = -br; j <= br; j++){
+                if(i * i + j * j <= br * br){
+                    SDL_RenderDrawPoint(renderer, ball.x + i, ball.y + j);
+                }
+        }
+    }
+
+
+    SDL_RenderPresent(renderer);
+    SDL_Delay(100);
 
     SDL_DestroyRenderer(renderer);
     SDL_DestroyWindow(window);
