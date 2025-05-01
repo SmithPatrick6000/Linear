be5a7a233fcb6c12f7e4645c02abe0f520a72c88
SmithPatrick6000c <patdousmi@gmail.com>
2025-05-01 00:14:57 -0400

    Points system added NOSCORE ON SCREEN

--- main.c
+++ main.c
@@ -20,11 +20,13 @@
 #include<stdio.h>
 #include<stdbool.h>
 #include<math.h>
+#include<stdlib.h>
+#include<time.h>
 
-void move(int amount);
+void move();
 void checkBounds();
 void updateAll();
-
+void defaultPos();
 
 const int SCREEN_WIDTH = 700;
 const int SCREEN_HEIGHT = 500;
@@ -33,6 +35,9 @@ const int PADDLE_HEIGHT = 200;
 const int LPADDLE_X = 100;
 const int RPADDLE_X = 580;
 
+int leftScore = 0;
+int rightScore = 0;
+int scoreDetection = 0;
 
 //creates Ball Structure
     typedef struct {
@@ -51,21 +56,39 @@ const int RPADDLE_X = 580;
 Paddle leftPaddle = {LPADDLE_X, PADDLE_HEIGHT, 20, 100, 0};
 Paddle rightPaddle = {RPADDLE_X, PADDLE_HEIGHT, 20, 100, 0};
 
-Ball ball = {400, 250, 20, 5, 5};
+Ball ball = {SCREEN_WIDTH/2, SCREEN_HEIGHT/2, 20, 5, 5};
 SDL_Renderer* renderer = NULL;
 
 
 int main(){
+    //Seed for rand
+    srand(time(NULL));
     //Initialized SDL
     SDL_Init(SDL_INIT_VIDEO);
     SDL_Window* window = SDL_CreateWindow("Ball Time", SDL_WINDOWPOS_UNDEFINED, SDL_WINDOWPOS_UNDEFINED,
                                 SCREEN_WIDTH, SCREEN_HEIGHT, SDL_WINDOW_SHOWN);
     renderer = SDL_CreateRenderer(window, -1, SDL_RENDERER_ACCELERATED);
+//Makes the game end after someone gets 5 points
+    while(leftScore != 5 && rightScore != 5){
+        //Makes the ball go in a random direction
+
+        int xrand = rand();
+        int yrand = rand();
+
+        ball.xSpeed = (rand() % 2 == 0) ? 5 : -5;
+        ball.ySpeed = (rand() % 2 == 0) ? 5 : -5;
+        //When someone scores it resets the screen
+        while(scoreDetection != 1){
+            move();
+        }
+       defaultPos();
+       scoreDetection = 0;
+    }
 
 
-
-    move(1000);
-
+    SDL_SetRenderDrawColor(renderer, 255, 255, 255, 255);
+    SDL_RenderClear(renderer);
+    SDL_RenderPresent(renderer);
 
     SDL_Delay(100);
     //Should Hack it to stay up May need to comment
@@ -87,13 +110,13 @@ int main(){
  *
  */
 
-void move(int amount){
-    for (int i = 0; i < amount;i++){
-        updateAll();
-        ball.x += ball.xSpeed;
-        ball.y += ball.ySpeed;
-        checkBounds();
-    }
+void move(){
+
+    updateAll();
+    ball.x += ball.xSpeed;
+    ball.y += ball.ySpeed;
+    checkBounds();
+
 }
 /*Clears the renderer
 Draws the ball using the ball radius and ball location
@@ -130,10 +153,16 @@ void updateAll(){
     SDL_Delay(5);
     SDL_RenderPresent(renderer);
 }
-/*Checks if the balls radius + the balls location is equal to the bounds of the machine
-If it is it reverses the speed*/
+/** \brief
+ *
+ * Checks if the ball is going to bounce off the walls or Paddles
+ *
+ *
+ *
+ */
+
 void checkBounds(){
-    //Game Section
+    //Defines the edges of the ball
     int ballR = ball.radius + ball.x;
     int ballL = -ball.radius + ball.x;
 
@@ -141,13 +170,21 @@ void checkBounds(){
     int ballB = ball.radius + ball.y;
 
 
-
+    //Detects if the ball hits the right or left side of the screen
     if(ballR >= SCREEN_WIDTH || -ball.radius + ball.x <= 0){
-        ball.xSpeed = -ball.xSpeed;
+        if(ballR >= SCREEN_WIDTH){
+            leftScore += 1;
+        }
+        else{
+            rightScore += 1;
+        }
+        scoreDetection += 1;
     }
+    //Detects if the ball hits the top or bottom of the screen
     else if(ballB >= SCREEN_HEIGHT || ballT <= 0){
         ball.ySpeed = -ball.ySpeed;
     }
+    //Detects if the ball hits either paddle
     else{
         if(ballR >= rightPaddle.x && ballL <= rightPaddle.x + 20 && ballT <= rightPaddle.y + 100 && ballB >= rightPaddle.y){
 
@@ -162,6 +199,33 @@ void checkBounds(){
     }
 
 
+}
+/** \brief Sets Ball and Paddles to starting positions
+ *
+ */
+void defaultPos(){
+
+    ball.x = SCREEN_WIDTH/2;
+    ball.y = SCREEN_HEIGHT/2;
+    ball.radius = 20;
+    ball.xSpeed = 5;
+    ball.ySpeed = 5;
+
+
+    leftPaddle.x = LPADDLE_X;
+    leftPaddle.y = PADDLE_HEIGHT;
+    leftPaddle.width = 20;
+    leftPaddle.length = 100;
+    leftPaddle.ySpeed = 0;
+
+
+    rightPaddle.x = RPADDLE_X;
+    rightPaddle.y = PADDLE_HEIGHT;
+    rightPaddle.width = 20;
+    rightPaddle.length = 100;
+    rightPaddle.ySpeed = 0;
+
+    updateAll();
 }
 
 
