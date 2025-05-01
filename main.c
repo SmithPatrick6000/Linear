bd8f5a3386438a8d4593c41420fbdcc7f5a118f4
SmithPatrick6000c <patdousmi@gmail.com>
2025-04-30 21:06:26 -0400

    Ball do be moving

--- main.c
+++ main.c
@@ -3,31 +3,54 @@
 #include<stdio.h>
 #include<stdbool.h>
 
+void move(int amount);
+void checkBounds();
+void updateBall();
+
+
 const int SCREEN_WIDTH = 700;
 const int SCREEN_HEIGHT = 500;
-int main(){
-    //Initialized SDL
-    SDL_Init(SDL_INIT_VIDEO);
-    SDL_Window* window = SDL_CreateWindow("Ball Time", SDL_WINDOWPOS_UNDEFINED, SDL_WINDOWPOS_UNDEFINED,
-                                SCREEN_WIDTH, SCREEN_HEIGHT, SDL_WINDOW_SHOWN);
-    SDL_Renderer* renderer = SDL_CreateRenderer(window, -1, SDL_RENDERER_ACCELERATED);
-    //Should Hack it to stay up May need to comment
-    /*SDL_Event e; bool quit = false; while(quit == false){
-            while(SDL_PollEvent(&e)){if(e.type == SDL_QUIT) quit = true;}}
-    */
 
-    //creates Ball Structure
+
+//creates Ball Structure
     typedef struct {
         int x, y;
         int radius;
         int xSpeed, ySpeed;
     } Ball;
-    Ball ball = {400, 300, 20, 5, 5};
+Ball ball = {400, 300, 20, 5, 5};
+SDL_Renderer* renderer = NULL;
 
-    //Game Section
+int main(){
+    //Initialized SDL
+    SDL_Init(SDL_INIT_VIDEO);
+    SDL_Window* window = SDL_CreateWindow("Ball Time", SDL_WINDOWPOS_UNDEFINED, SDL_WINDOWPOS_UNDEFINED,
+                                SCREEN_WIDTH, SCREEN_HEIGHT, SDL_WINDOW_SHOWN);
+    renderer = SDL_CreateRenderer(window, -1, SDL_RENDERER_ACCELERATED);
+
+    move(1000);
 
 
+    SDL_Delay(100);
+    //Should Hack it to stay up May need to comment
+    //SDL_Event e; bool quit = false; while(quit == false){
+     //       while(SDL_PollEvent(&e)){if(e.type == SDL_QUIT) quit = true;}}
 
+
+    SDL_DestroyRenderer(renderer);
+    SDL_DestroyWindow(window);
+    SDL_Quit();
+    return 0;
+}
+void move(int amount){
+    for (int i = 0; i < amount;i++){
+        updateBall();
+        ball.x += ball.xSpeed;
+        ball.y += ball.ySpeed;
+        checkBounds();
+    }
+}
+void updateBall(){
     //Clear Screen
     SDL_SetRenderDrawColor(renderer, 0, 0, 0, 255);
     SDL_RenderClear(renderer);
@@ -41,15 +64,16 @@ int main(){
                 }
         }
     }
-
-
     SDL_RenderPresent(renderer);
-    SDL_Delay(100);
-
-    SDL_DestroyRenderer(renderer);
-    SDL_DestroyWindow(window);
-    SDL_Quit();
-    return 0;
+}
+void checkBounds(){
+    //Game Section
+    if(ball.radius + ball.x >= SCREEN_WIDTH || ball.radius + ball.x <= 0){
+        ball.xSpeed = -ball.xSpeed;
+    }
+    if(ball.radius + ball.y >= SCREEN_HEIGHT || ball.radius + ball.y <= 0){
+        ball.ySpeed = -ball.ySpeed;
+    }
 }
 
 
